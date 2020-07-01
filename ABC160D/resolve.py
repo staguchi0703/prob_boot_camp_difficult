dac0d9cd1e3f218a2e39a672c0667e0d2ca49b5d
sho Taguchi <s.taguchi0703@gmail.com>
2020-07-01 23:36:13 +0900

    ABC160Dようやく解けた

--- ABC160D/resolve.py
+++ ABC160D/resolve.py
@@ -3,37 +3,38 @@ def resolve():
     code here
     '''
     import collections
-    N, X, Y = [int(i) for i in input().split()]
+    N, X, Y = [int(item) for item in input().split()]
+    X -= 1
+    Y -= 1
 
     edges = [[] for _ in range(N)]
-    fp = [0 for _ in range(N)]
-
     for i in range(N-1):
         edges[i].append(i+1)
         edges[i+1].append(i)
+    edges[X].append(Y)
+    edges[Y].append(X)
 
-    edges[X-1].append(Y-1)
-    edges[Y-1].append(X-1)
 
+    fp = [-1 for _ in range(N)]
 
-    cnt = 0
-    for k in range(N-1):
-        for i in range(N):
-            que = collections.deque([edges[i]])
-            dist = 0
-            while que:
-                temp = que.popleft()
-                dist += 1
-                if dist == k:
-                    cnt += 1
-                    que = False
-                else:
-                    print(temp)
-                    # for j in temp:
-                    #     if fp[j] != 1:
-                    #         que.append(j)
-                    #         fp[j] = 1
-    print(cnt)
+    res_memo = [0 for _ in range(N)]
+    for i in range(N):
+        fp = [-1 for _ in range(N)]
+        que = collections.deque([[i, 0]])
+        fp[i] = 0
+        while que:
+            node, dist = que.popleft()
+            for to_node in edges[node]:
+                if fp[to_node] == -1:
+                    que.append([to_node, dist+1])
+                    fp[to_node] = dist+1
+
+        for item in fp:
+            res_memo[item] +=1
+    
+    for item in res_memo[1:]:
+        print(item//2)
+
 
 
 if __name__ == "__main__":
--- ABC160D/test_pasted_from_page.py
+++ ABC160D/test_pasted_from_page.py
@@ -4,5 +4,62 @@ from resolve import resolve
 ####################################
 # 以下にプラグインの内容をペーストする
 #  
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        print(out)
+        print('------------')
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """5 2 4"""
+        output = """5
+4
+1
+0"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """3 1 3"""
+        output = """3
+0"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """7 3 7"""
+        output = """7
+8
+4
+2
+0
+0"""
+        self.assertIO(input, output)
+
+    def test_入力例_4(self):
+        input = """10 4 8"""
+        output = """10
+12
+10
+8
+4
+1
+0
+0
+0"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
 
 
