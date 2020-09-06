f859d24ab347efe0155791de0a3136a21324f723
sho Taguchi <s.taguchi0703@gmail.com>
2020-09-06 16:06:01 +0900

    aaa

--- z copy 4/resolve.py
+++ z copy 4/resolve.py
@@ -3,6 +3,13 @@ def resolve():
     code here
     '''
 
+    N = int(input())
+    As = [int(item) for item in input().split()]
+
+    def cal(num):
+        res_list = [0 for _ in range(N)]
+        
+
 
 if __name__ == "__main__":
     resolve()
--- z copy 4/test_pasted_from_page.py
+++ z copy 4/test_pasted_from_page.py
@@ -4,3 +4,39 @@ from resolve import resolve
 ####################################
 # 以下にプラグインの内容をペーストする
 #  
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """3
+2 2 4"""
+        output = """4 0 4"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """5
+3 8 7 5 5"""
+        output = """2 4 12 2 8"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """3
+1000000000 1000000000 0"""
+        output = """0 2000000000 0"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
